8ca14c90c581e98cf6845acd8ace560d46bf71a8
Wilnane <wilnane-venezia@hotmail.com>
2025-06-26 23:18:10 -0300

    Add files via upload

---
+++ scriptindex.js
@@ -0,0 +1,14 @@
+let botao = document.querySelector ("#botao");
+botao.style.background="lightblue";
+let mensagem = document.querySelector("#mensagem");
+
+
+
+{
+
+botao.addEventListener("click",e =>{
+mensagem.innerHTML="Seja bem vido a nossa página. Não esqueça de tomar água.";
+ 
+
+});
+}
